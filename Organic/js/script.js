90bd4ece9437c610e8cedd2460ad4c0881058403
flizerzet <danyok.p@gmail.com>
2020-11-05 16:05:35 +0300

    finished desktop version

--- Organic/js/script.js
+++ Organic/js/script.js
@@ -22,4 +22,20 @@ $(document).ready(function(){
 		  }
 		]
 	});
+	$(document).ready(function() {
+		$('.slider-new-counter__minus').click(function () {
+			var $input = $(this).parent().find('input');
+			var count = parseInt($input.val()) - 1;
+			count = count < 1 ? 1 : count;
+			$input.val(count);
+			$input.change();
+			return false;
+		});
+		$('.slider-new-counter__plus').click(function () {
+			var $input = $(this).parent().find('input');
+			$input.val(parseInt($input.val()) + 1);
+			$input.change();
+			return false;
+		});
+	});
 });
